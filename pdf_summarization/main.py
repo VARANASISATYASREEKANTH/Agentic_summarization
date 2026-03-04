5ee4866f287ac7635a6d674d00692e620264e959
Varanasi Satya Sreekanth <86218986+VARANASISATYASREEKANTH@users.noreply.github.com>
2026-03-04 16:49:23 +0530

    Change PDF path in main.py
    Updated the PDF path for the summarization process.

--- pdf_summarization/main.py
+++ pdf_summarization/main.py
@@ -14,9 +14,10 @@ if __name__ == "__main__":
 
     agent = PDFSummarizationAgent(api_key=api_key)
 
-    pdf_path = "C:\my_projects\melco_DATA\video_mix_aug_seminal_paper.pdf"
+    pdf_path = "C:\my_projects\video_mix_aug_seminal_paper.pdf"
 
     summary = agent.summarize_pdf(pdf_path)
 
     print("\n\n===== FINAL SUMMARY =====\n")
+
     print(summary)
